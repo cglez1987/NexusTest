6a8c5ac8e82bac43bbeda3604c8cd9595bd64aa0
carlogon <cglez1987@gmail.com>
2017-09-17 22:00:00 -0400

    Primeros cambios en Nexus test

---
+++ NexusTest/src/main/java/py/com/cglez1987/nexustest/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package py.com.cglez1987.nexustest;
+
+/**
+ *
+ * @author carlos
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        System.out.println("Probando la integracion de Nexus con Jenkins");
+    }
+    
+}
